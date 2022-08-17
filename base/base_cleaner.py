792d36da87dafc66ec2838cf7ffa06c36a739948
Prajwal Patil <prajwalpatilk@gmail.com>
2022-08-17 09:17:43 +0530

    Join seperate keys

--- base/base_cleaner.py
+++ base/base_cleaner.py
@@ -1,24 +1,40 @@
 import json
 
 copy_base = {}
-
+knowledge_base = {}
+knowledge_base_string = {}
 #Reading from the json file
-with open("json/base.json") as json_file:
+with open("./json/base.json","r") as json_file:
     knowledge_base = json.load(json_file)    
 
 
 #Converting array to set to remove duplicates  
-#And thenfrom set to array for JSON serialization.
+#And then from set to array for JSON serialization.
 for i in knowledge_base:
     print(i)
     copy_base[i] = []
     for j in knowledge_base[i]:
         temp_set = set(j)
         copy_base[i].append(list(temp_set))
-
+for h in knowledge_base:
+    knowledge_base_string[h] = []
+    for h_array in knowledge_base[h]:
+        knowledge_base_string[h].append([])
+        base_string = " "
+        for keys in h_array:
+            keys = keys.lower()
+            base_string = base_string + ' ' + keys
+        knowledge_base_string[h][-1] = base_string
+        
 #Dumping the processed file
 try:
     with open("json/base_refined.json", "w") as write_file:
         json.dump(copy_base, write_file, indent=4)
+except:
+    print("Error in dumping JSON!")
+
+try:
+    with open("./json/base_joined.json", "w") as write_file:
+        json.dump(knowledge_base_string, write_file, indent=4)
 except:
     print("Error in dumping JSON!")
